54240c1f26f1470d5a2b8253d9d70157c6d5dcac
Jordan Keating <jordan@brakelazy.com>
2016-08-21 18:45:27 -0700

    removes drip on mobile

--- mustard.js
+++ mustard.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   $("#playButton").click(function() {
     $('#dripper').addClass('drip');
 
-    var delay=1400;
+    var delay=1500;
     setTimeout(function() {
       $('#dripper').removeClass('drip');
     }, delay);
